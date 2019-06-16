5d0b3a6184a2d4582a180ca8cf745690047812d8
TheKevSter35 <43183768+TheKevSter35@users.noreply.github.com>
2019-06-16 23:52:49 +0200

    Add files via upload
    returned search.js file

---
+++ search.js
@@ -0,0 +1,46 @@
+// Require dependencies
+const camelCase = require('camel-case');
+const express = require('express');
+const app = express();
+const port = 3000;
+const slug = require('slug');
+const bodyParser = require('body-parser');
+const multer = require('multer');
+const find = require('array-find');
+const mongo = require('mongodb');
+const session = require('express-session');
+var upload = multer({ dest: 'static/upload/' });
+var db = null;
+require('dotenv').config();
+var url = process.env.DB_HOST
+mongo.MongoClient.connect(url, function(err, client) {
+  if (err)
+    throw err
+  db = client.db(process.env.DB_NAME)
+});
+
+// Function
+function search(req, res) {
+
+  if (!req.session.user) {
+
+    return res.redirect('/')
+    
+  } else {
+    db.collection('data').find().toArray(done);
+
+    function done(err, data) {
+      if (err) {
+        next(err)
+      } else {
+        res.render('search', {
+          data: data,
+          user: req.session.user,
+          title: "Search for Interests"
+        });
+      }
+    }
+  }
+}
+module.exports = search;
+
